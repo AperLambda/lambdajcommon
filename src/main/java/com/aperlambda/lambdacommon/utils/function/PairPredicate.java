8cf5969303047aaff1c1990e423fb0b7bf78f6d6
wAperClem <aurora42lambda@gmail.com>
2017-10-11 15:17:03 +0200

    Add PairPredicate

---
+++ src/main/java/com/aperlambda/lambdacommon/utils/function/PairPredicate.java
@@ -0,0 +1,27 @@
+package com.aperlambda.lambdacommon.utils.function;
+
+@FunctionalInterface
+public interface PairPredicate<X, Y>
+{
+    /**
+     * Evaluates this predicate on the given argument.
+     *
+     * @param x The first input argument.
+     * @param y The second input argument.
+     * @return {@code true} if the input argument matches the predicate,
+     * otherwise {@code false}.
+     */
+    boolean test(X x, Y y);
+
+    /**
+     * Returns a predicate that represents the logical negation of this
+     * predicate.
+     *
+     * @return A predicate that represents the logical negation of this
+     * predicate.
+     */
+    default PairPredicate<X, Y> negate()
+    {
+        return (x, y) -> !test(x, y);
+    }
+}
