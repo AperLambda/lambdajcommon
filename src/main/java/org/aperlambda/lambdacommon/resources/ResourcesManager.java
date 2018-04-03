d14bce41baf20a258d02c6293f9351213bbe4261
LambdAurora <aurora42lambda@gmail.com>
2018-04-03 21:42:34 +0200

    Updated copyright headers and LICENSE

--- src/main/java/org/aperlambda/lambdacommon/resources/ResourcesManager.java
+++ src/main/java/org/aperlambda/lambdacommon/resources/ResourcesManager.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright © 2018 AperLambda <aper.entertainment@gmail.com>
+ *
+ * This file is part of λjcommon.
+ *
+ * Licensed under the MIT license. For more information,
+ * see the LICENSE file.
+ */
+
 package org.aperlambda.lambdacommon.resources;
 
 import org.aperlambda.lambdacommon.utils.Optional;
