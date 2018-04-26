a42e5354e870bf266f342b94cd1a0d4857679040
LambdAurora <aurora42lambda@gmail.com>
2018-04-26 20:43:36 +0200

    :floppy_disk: λjcommon v1.4.3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,13 +7,13 @@
  * see the LICENSE file.
  */
 
-module org.aperlambda.lambdacommon {
+open module org.aperlambda.lambdacommon {
 	exports org.aperlambda.lambdacommon;
-	opens org.aperlambda.lambdacommon.utils;
 	exports org.aperlambda.lambdacommon.config;
 	exports org.aperlambda.lambdacommon.documents;
 	exports org.aperlambda.lambdacommon.resources;
 	exports org.aperlambda.lambdacommon.system;
+	exports org.aperlambda.lambdacommon.utils;
 	exports org.aperlambda.lambdacommon.utils.function;
 
 	requires java.desktop;
